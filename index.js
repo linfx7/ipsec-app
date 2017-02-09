ff8989c53a877335db102b5b313927057c1952fd
linfx7 <linfx7@gmail.com>
2017-02-09 11:17:46 +0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var express = require('express');
+var request = require('request');
+
+var apiUrl = 'http://www.cnbeta.com/';
+var app = express();
+
+// static items
+app.use('', express.static(__dirname + '/views'));
+app.use('/static', express.static(__dirname + '/static'));
+
+//redirect rest request
+app.use('/api', function(req, res) {
+    var url = apiUrl + req.url;
+    console.log('Request: ' + url);
+    req.pipe(request({qs: req.query, uri: url})).pipe(res);
+});
+
+http.createServer(app).listen('80', function() {
+    console.log('Server started !');
+});
---
+++ static/test.js
@@ -0,0 +1,3 @@
+function a321() {
+    alert("hello");
+}
