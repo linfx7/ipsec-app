99673bdb74fa0d248dc6065043fd2ccfe3d3a85f
sunshiny92 <1119514881@qq.com>
2017-02-16 15:43:37 +0800

    暂时就这样

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ app.use('/static', express.static(__dirname + '/static'));
 app.use('/templates', express.static(__dirname + '/templates'));
 
 //redirect rest request
-app.use('/api', function(req, res) {
+app.use('/api', function (req, res) {
     var url = apiUrl + req.url;
     // console.log('Request: ' + url);
-    req.pipe(request({qs: req.query, uri: url})).pipe(res);
+    req.pipe(request({
+        qs: req.query,
+        uri: url
+    })).pipe(res);
 });
 
-http.createServer(app).listen('80', function() {
+http.createServer(app).listen('8080', function () {
     console.log('Server started !');
 });
